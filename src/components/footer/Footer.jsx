ee2c7d109f1c9c094e3c626578133980aee5b520
vintech05 <kelanjutannyakelvin@gmail.com>
2023-11-04 22:19:05 +0700

    added decoration

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,8 +13,13 @@ const Footer = () => {
     return (
         <div>
             <div className='flex flex-row justify-between items-center px-6 border-t-2 border-button-clr'>
-            <div className='text-button-clr font-Manrope text-sm font-semibold text-center p-8'>
+            <div className='text-button-clr flex flex-col font-Manrope text-sm font-semibold text-center p-8'>
             <span>Copyright &copy; 2023 Kelvin</span>
+            <div className='flex flex-row gap-2 pt-2'>
+            <div className="w-4 h-4 bg-button-clr rounded-full"></div>
+            <div className="w-4 h-4 border-4 border-button-clr rounded-full"></div>
+            <div className="w-4 h-4 border-2 border-button-clr rounded-full"></div>
+            </div>
             </div>
             <div onClick={scrollToTop} className='group p-2 border-2 border-button-clr hover:bg-button-clr cursor-pointer'>
                 <a href="#">
