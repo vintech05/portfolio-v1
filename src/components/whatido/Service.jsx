eaf734ba5b44688fb6523fa36773678f90034558
vintech05 <kelanjutannyakelvin@gmail.com>
2023-11-08 22:41:19 +0700

    polishing text structure

--- src/components/whatido/Service.jsx
+++ src/components/whatido/Service.jsx
@@ -15,13 +15,13 @@ const Service = () => {
                 <div id='card-1' className='flex flex-col items-center gap-2 border-2 border-button-clr px-8 py-12 lg:px-10 lg:py-24'>
                     <RiGlobalLine className='text-4xl text-button-clr lg:text-5xl'/>
                     <span className='text-button-clr text-2xl font-Manrope capitalize font-semibold pb-6'>web development</span>
-                    <p className='font-Manrope text-button-clr text-sm break-words text-left leading-6 lg:break-all lg:w-[25em]'>I provide comprehensive solutions tailored to your needs. I specialize in creating dynamic, user-friendly websites using the latest technologies and frameworks. My services include front-end and website optimization. I adhere to best practices in coding, ensuring your website is not only visually appealing but also efficient and secure.</p>
+                    <p className='font-Manrope text-button-clr text-sm break-words text-left leading-6 lg:break-before-all lg:w-[25em]'>I provide comprehensive solutions tailored to your needs. I specialize in creating dynamic, user-friendly websites using the latest technologies and frameworks. My services include front-end and website optimization. I adhere to best practices in coding, ensuring your website is not only visually appealing but also efficient and secure.</p>
                         <span className='text-button-clr font-Manrope font-semibold text-sm mt-6 lg:pr-[19.6em] cursor-pointer hover:underline'>Learn More</span>
                 </div>
                 <div id='card-2' className='flex flex-col items-center gap-2 bg-button-clr px-8 py-12 lg:px-10 lg:py-24'>
                     <BiPaintRoll className='text-4xl text-bg-clr lg:text-5xl'/>
                     <span className='text-bg-clr text-2xl font-Manrope capitalize font-semibold pb-6'>web design</span>
-                    <p className='font-Manrope text-bg-clr text-sm break-words text-left leading-6 lg:break-all lg:w-[25em]'>I focus on creating visually appealing and user-friendly websites that meet your specific needs. I use the latest design trends and technologies to ensure your website is modern and responsive. My services include UI/UX design, graphic design, and website layout planning. I prioritize user experience, ensuring that your website is not only beautiful but also easy to navigate.</p>
+                    <p className='font-Manrope text-bg-clr text-sm break-words text-left leading-6 lg:break-before-all lg:w-[25em]'>I focus on creating visually appealing and user-friendly websites that meet your specific needs. I use the latest design trends and technologies to ensure your website is modern and responsive. My services include UI/UX design, graphic design, and website layout planning. I prioritize user experience, ensuring that your website is not only beautiful but also easy to navigate.</p>
                         <span className='text-bg-clr font-Manrope font-semibold text-sm mt-6 lg:pr-[19.6em] cursor-pointer hover:underline'>Learn More</span>
                 </div>
             </div>
