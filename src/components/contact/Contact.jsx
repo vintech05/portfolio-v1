38cc4f59f5697707ffb8597205c15e59761ee23a
vintech05 <kelanjutannyakelvin@gmail.com>
2023-11-12 21:19:15 +0700

    added necessary breakpoints for padding bottom

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -30,7 +30,7 @@ const Contact = () => {
 
     return (
         <div className='grid place-items-center h-screen mt-32'>
-            <div className='pb-24 lg:pb-8 grid place-items-center'>
+            <div className='pb-24 md:pb-12 grid place-items-center'>
                 <h1 className='font-serif text-4xl text-button-clr uppercase'>lets connect!</h1>
                 <LiaHandshake className='font-Manrope text-3xl text-button-clr uppercase'/>
             </div>
