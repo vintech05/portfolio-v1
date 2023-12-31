d81fbe1126ffd8308774e210dc1ba7b4cf9770df
vintech05 <kelanjutannyakelvin@gmail.com>
2023-12-31 19:16:48 +0700

    updated copyright

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,17 +4,17 @@ import {AiOutlineArrowUp} from 'react-icons/ai'
 const Footer = () => {
 
     const scrollToTop = () => {
-        window.scrollTo({
-          top: 0,
-          behavior: "smooth"
-        });
-      };
+            window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+            });
+        };
 
     return (
         <div>
             <div className='flex flex-row justify-between items-center px-6 border-t-2 border-button-clr'>
             <div className='text-button-clr flex flex-col font-Manrope text-sm font-semibold text-center p-8'>
-            <span>Copyright &copy; 2023 Kelvin</span>
+            <span>Copyright &copy; 2024 Kelvin</span>
             <div className='flex flex-row gap-2 pt-2'>
             <div className="w-4 h-4 bg-button-clr rounded-full"></div>
             <div className="w-4 h-4 border-4 border-button-clr rounded-full"></div>
