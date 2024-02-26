9a9c872d4df7760710daa013b5d198b8b640f3d9
vintech05 <kelanjutannyakelvin@gmail.com>
2024-02-26 18:10:16 +0700

    fixed text not aligning properly

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,14 +13,14 @@ const Footer = () => {
     return (
         <div>
             <div className='flex flex-row justify-between items-center px-6 border-t-2 border-button-clr'>
-            <div className='text-button-clr flex flex-col font-Manrope text-sm font-semibold text-center p-8'>
-            <span>Copyright &copy; 2024 Kelvin</span>
-            <div className='flex flex-row gap-2 pt-2'>
-            <div className="w-4 h-4 bg-button-clr rounded-full"></div>
-            <div className="w-4 h-4 border-4 border-button-clr rounded-full"></div>
-            <div className="w-4 h-4 border-2 border-button-clr rounded-full"></div>
-            </div>
-            </div>
+                <div className='text-button-clr text-left flex flex-col font-Manrope text-sm font-semibold p-8'>
+                    <span>Copyright &copy; 2024 Kelvin</span>
+                <div className='flex flex-row gap-2 pt-2'>
+                    <div className="w-4 h-4 bg-button-clr rounded-full"></div>
+                    <div className="w-4 h-4 border-4 border-button-clr rounded-full"></div>
+                    <div className="w-4 h-4 border-2 border-button-clr rounded-full"></div>
+                </div>
+                </div>
             <div onClick={scrollToTop} className='group p-2 border-2 border-button-clr hover:bg-button-clr cursor-pointer'>
                 <a href="#">
                     <AiOutlineArrowUp className='group text-2xl text-button-clr group-hover:text-bg-clr transition-all'/>
